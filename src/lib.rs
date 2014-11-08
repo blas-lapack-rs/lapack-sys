5ba154d0633ba0d5281e40513a48ac56ed651ede
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-08 15:20:25 +0100

    Capitalized parameter names to match the offical reference

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! The library facilitates static linking with the [Linear Algebra PACKage][1].
+//! Facilitation of static linking with the [Linear Algebra PACKage][1].
 //!
 //! [1]: http://www.netlib.org/lapack/
 
@@ -14,7 +14,7 @@ pub use dsyev_ as dsyev;
 #[link(name = "gfortran")]
 extern {
     /// http://www.netlib.org/lapack/explore-html/dd/d4c/dsyev_8f.html
-    fn dsyev_(jobz: *const c_char, uplo: *const c_char, n: *const c_int,
-              a: *mut c_double, lda: *const c_int, w: *mut c_double,
-              work: *mut c_double, lwork: *const c_int, info: *mut c_int);
+    fn dsyev_(JOBZ: *const c_char, UPLO: *const c_char, N: *const c_int,
+              A: *mut c_double, LDA: *const c_int, W: *mut c_double,
+              WORK: *mut c_double, LWORK: *const c_int, INFO: *mut c_int);
 }
