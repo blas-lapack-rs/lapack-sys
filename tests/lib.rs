fa1537ad6689641e671955067091acad8441a2e1
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-19 18:12:25 -0400

    Update the test

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,19 +2,18 @@ extern crate lapack_sys as raw;
 
 #[test]
 fn link() {
-    let mut jobz = b'V' as i8;
-    let mut uplo = b'U' as i8;
-    let mut n = 1;
+    let jobz = b'V' as i8;
+    let uplo = b'U' as i8;
+    let n = 1;
     let mut a = vec![0.0];
-    let mut lda = 1;
+    let lda = 1;
     let mut w = vec![0.0];
     let mut work = vec![0.0, 0.0];
-    let mut lwork = 2;
+    let lwork = 2;
     let mut flag = 0;
 
     unsafe {
-        raw::dsyev_(&mut jobz as *mut _, &mut uplo as *mut _, &mut n as *mut _, a.as_mut_ptr(),
-                    &mut lda as *mut _, w.as_mut_ptr(), work.as_mut_ptr(), &mut lwork as *mut _,
-                    &mut flag as *mut _);
+        raw::dsyev_(&jobz, &uplo, &n, a.as_mut_ptr(), &lda, w.as_mut_ptr(), work.as_mut_ptr(),
+                    &lwork, &mut flag);
     }
 }
