5c603b425f3a4210ccfcb5df34fbce2b711670f0
Ivan Ukhov <ivan.ukhov@gmail.com>
2021-04-03 07:48:27 +0200

    Polish the bindgen script

--- bindgen.sh
+++ bindgen.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
+
 set -eux
 
-# - LAPACK routines in lapack.h has `.*_` naming because `LAPACK_GLOBAL` macro adds last '_'
-#   This script filters using this last '_'.
+# * The names of the LAPACK routines in `lapack.h` end with an underscore, which
+#   is how they are identified among other functions.
 #
-# - `char` in C header will convert into `::std::os::raw::c_char`, but we want to keep no_std.
-#   This is replaced by sed (FIXME is it possible using bindgen?).
-bindgen \
-  --whitelist-function="^.*_$" \
-  --use-core \
-  wrapper.h \
-  | sed -e "s/::std::os::raw:://g" \
+# * By default, `char` is converted to `::std::os::raw::c_char`. However, to
+#   preserve no_std, it is truncated to `c_char` and gets taken from `libc`.
+bindgen --whitelist-function='^.*_$' --use-core wrapper.h \
+  | sed -e 's/::std::os::raw:://g' \
   > src/lapack.rs
-# Re-format due to sed
+
 rustfmt src/lapack.rs
